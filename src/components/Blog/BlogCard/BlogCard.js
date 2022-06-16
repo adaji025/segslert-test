ca9d90396ef3452375f6367b8c1db273f3290487
Adaji Mukhtar <alfadaji@gmail.com>
2022-06-16 17:53:25 +0100

    completed blog component

--- src/components/Blog/BlogCard/BlogCard.js
+++ src/components/Blog/BlogCard/BlogCard.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import styles from './BlogCard.module.scss'
+import React from "react";
+import { FaArrowRight } from 'react-icons/fa'
+import styles from "./BlogCard.module.scss";
+
+import house2 from "../../../assets/images/house2.jpg";
 
 const BlogCard = () => {
   return (
-    <div className={styles.card}>BlogCard</div>
-  )
-}
+    <div className={styles.card}>
+      <div className={styles.card__image}>
+        <img src={house2} alt="" />
+      </div>
+      <div className={styles.card__desc}>
+        <h2>Why Live in London</h2>
+        <p>May 05, 2021</p>
+        <span>Realtors, you know cant be good at your jobs if you not pluged into the industry</span>
+        <div className={styles.card__desc__read}>
+            <p>Read More</p>
+            <FaArrowRight />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BlogCard
+export default BlogCard;
