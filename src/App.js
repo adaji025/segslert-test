098de587521bef4d170a79debb04b164242bd73f
Adaji Mukhtar <alfadaji@gmail.com>
2022-06-18 16:52:00 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import './styles/index.scss'
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Services from './components/Services/Services';
-import Property from './components/Property/Property';
-import Featured from './components/Featured/Featured';
-import Blog from './components/Blog/Blog';
-import Testimonial from './components/Testimonial/Testimonial';
-import { Footer } from './components/Footer/Footer';
+import "./styles/index.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Property from "./components/Property/Property";
+import Featured from "./components/Featured/Featured";
+import Blog from "./components/Blog/Blog";
+import Testimonial from "./components/Testimonial/Testimonial";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <Hero />
-      <Services />
-      <Property />
+      <div id="home">
+        <Hero />
+      </div>
+      <div id="services">
+        <Services />
+      </div>
+      <div id="about">
+        <Property />
+      </div>
       <Featured />
-      <Blog />
-      <Testimonial />
+      <div id="blog">
+        <Blog />
+      </div>
+      <div id="contact">
+        <Testimonial />
+      </div>
       <Footer />
     </div>
   );
